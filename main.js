d7657055b173359dfdecdc5da3720236ca3c1bf6
Robert S <112721454+roberts-fullstack@users.noreply.github.com>
2022-11-27 17:04:36 -0500

    updates

--- main.js
+++ main.js
@@ -1,8 +1,47 @@
 // Your code here.
 
+function getFirstName(usr){
+  return usr.firstName;
+}
 
+function getLastName(usr){
+  return usr.lastName;
+}
 
+function getFullName(usr){
+  return `${usr.firstName} ${usr.lastName}`;
+}
 
+function setFirstName(usr, str){
+  usr.firstName = str;
+}
+
+function setAge(usr, num){
+  usr.age = num;
+}
+
+function giveBirthday(usr){
+  if (usr.age === undefined){
+    usr.age = 1;
+  } else {
+    usr.age += 1;
+  }
+  usr;
+}
+
+function marry(usr1, usr2){
+  usr1.married = true;
+  usr2.married = true;
+  usr1.spouseName = getFullName(usr2);
+  usr2.spouseName = getFullName(usr1);
+}
+
+function divorce(usr1, usr2){
+  usr1.married = false;
+  delete usr1.spouseName;
+  usr2.married = false;
+  delete usr2.spouseName;
+}
 
 // Our code here. Don't touch!
 if (typeof getFirstName === 'undefined') {
